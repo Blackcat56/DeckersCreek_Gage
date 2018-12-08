62f57a2d0f4674ec8ccbfa485548515b9df3abce
Amanda Adams <aeadams@mix.wvu.edu>
2018-12-07 22:19:18 -0500

    added a small bit to the code

--- deckers.sh
+++ deckers.sh
@@ -3,8 +3,6 @@ for gauge in "03062500"
 do
 	wget -O "$gauge".txt "https://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=03062500&referred_module=sw&period=&begin_date=1946-03-03&end_date=2018-12-06"
 	sed -n '17p' "$gauge".txt >> flow.txt
-	tail -n 1 "$gauge".txt | cut -d$'\t' -f 3,5 >> flows_out.txt
-
+	cut -d$'\t' -f 3,4,5 "$gauge".txt>> flows_out.txt
 done
 
-wget -O "https://waterdata.usgs.gov/nwis/dv?cb_00060=on&format=rdb&site_no=03062500&referred_module=sw&period=&begin_date=1946-03-03&end_date=2018-12-06"
