e3175024af70b293c668a541af0844908cc31e17
Amanda Adams <aeadams@mix.wvu.edu>
2018-12-10 21:48:22 -0500

    Why why?

--- Rivers.sh
+++ Rivers.sh
@@ -1,6 +1,6 @@
-for gauge in "03062500" $1
+for gauge in "03062224" "03062225" "03062235" "03042245" "03063350" "03062500" "03071590"
 do
-	wget -O "$gauge".txt "https://nwis.waterdata.usgs.gov/wv/nwis/uv/?cb_00060=on&cb_00065=on&format=rdb&site_no=03062500&period=&begin_date=1950-11-01&end_date=2018-12-03"
+	wget -O "$gauge".txt "https://nwis.waterdata.usgs.gov/wv/nwis/uv/?cb_00060=on&cb_00065=on&format=rdb&site_no=03062500&period=&begin_date=1948-11-01&end_date=2018-12-03"
 	sed -n '17p' "$gauge".txt >> flow.txt
 	tail -n 1 "$gauge".txt | cut -d$'\t' -f 3,5 >> flows_out.txt
 
