c5a1b7d176282aa1b68104213ccd2d13682ac88a
Amanda Adams <aeadams@mix.wvu.edu>
2018-12-14 20:34:28 -0500

    got something to work

--- stillploting.R
+++ stillploting.R
@@ -1,3 +1,4 @@
-rank<-as.data.frame(table(Streamflow))
+rank<-as.data.frame(table(Streamflow$`Stream flow`))
 #rank<-read.table((Streamflow$Rank),header = TRUE)
-ggplot(data=rank,aes(rank$Freq)) + geom_histogram()#this worked somewhat
+#This produces a histograms!!
+ggplot(data=rank,aes(rank$Freq)) + geom_histogram()
