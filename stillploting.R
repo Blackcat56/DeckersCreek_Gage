a77c5c44033e5a150ce15a1598709ceb4696461b
Amanda Adams <aeadams@mix.wvu.edu>
2018-12-15 10:43:34 -0500

    commenting the code

--- stillploting.R
+++ stillploting.R
@@ -1,6 +1,8 @@
+#Using ggplot2 to produce graphs from Deckers creek stream flow data.
+#This code creates the data frame used for the graph.
 rank<-as.data.frame(table(Streamflow$`Stream flow`))
-#rank<-read.table((Streamflow$Rank),header = TRUE)
-#This produces a histograms!!
+#These two varables determine the bar fill and bar line colors.   
 barfill<-"royalblue1"
 barlines<-"royalblue4"
-ggplot(data=rank,aes(rank$Freq)) + geom_histogram(aes(y=..count..), color =barlines, fill = barfill) + scale_x_continuous(name="Stream Flow amounts in cfs") + scale_y_continuous("Count") + ggtitle("Frequency histogram of the Stream Flow in cfs")
+#This is the code to create the histogram. 
+ggplot(data=rank,aes(rank$Freq)) + geom_histogram(aes(y=..count..), color = barlines, fill = barfill) + scale_x_continuous(name="Stream Flow amounts in cfs") + scale_y_continuous("Count") + ggtitle("Frequency histogram of the Stream Flow in cfs")
