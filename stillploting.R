75ebe6a374544349d0bf6eea41a751d4838b79d3
Amanda Adams <aeadams@mix.wvu.edu>
2018-12-15 10:33:57 -0500

    more work on plotting

--- stillploting.R
+++ stillploting.R
@@ -1,4 +1,6 @@
 rank<-as.data.frame(table(Streamflow$`Stream flow`))
 #rank<-read.table((Streamflow$Rank),header = TRUE)
 #This produces a histograms!!
-ggplot(data=rank,aes(rank$Freq)) + geom_histogram()+scale_x_continuous(name="Stream Flow amounts in cfs") + scale_y_continuous("Count") + ggtitle("Frequency histogram of the Stream Flow in cfs")
+barfill<-"royalblue1"
+barlines<-"royalblue4"
+ggplot(data=rank,aes(rank$Freq)) + geom_histogram(aes(y=..count..), color =barlines, fill = barfill) + scale_x_continuous(name="Stream Flow amounts in cfs") + scale_y_continuous("Count") + ggtitle("Frequency histogram of the Stream Flow in cfs")
