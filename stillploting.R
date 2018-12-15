bf1fe53f46c02800f53fc86fd8e4b3d4e51915c4
Amanda Adams <aeadams@mix.wvu.edu>
2018-12-15 10:58:58 -0500

    added the 'save' function for charts

--- stillploting.R
+++ stillploting.R
@@ -6,3 +6,7 @@ barfill<-"royalblue1"
 barlines<-"royalblue4"
 #This is the code to create the histogram. 
 ggplot(data=rank,aes(rank$Freq)) + geom_histogram(aes(y=..count..), color = barlines, fill = barfill) + scale_x_continuous(name="Stream Flow amounts in cfs") + scale_y_continuous("Count") + ggtitle("Frequency histogram of the Stream Flow in cfs")
+
+#save the chart
+dev.copy(jpeg,'myplot.jpeg')
+dev.off()
